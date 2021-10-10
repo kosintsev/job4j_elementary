941f5f1ff963ec59f69174274efdd63b165986a6
kosintsev <andrejkosincev@gmail.com>
2021-10-10 18:42:08 +0700

    fixbug 4.3.6. Math.abs [#373616 #214262]

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -4,10 +4,9 @@ public class ChessBoard {
 
     public static int way(int x1, int y1, int x2, int y2) {
         int rsl = 0;
-        boolean isOut = x1 > 7 && y1 > 7 && x2 > 7 && y2 > 7;
-        boolean isNotPositive = (y1 * x2) * (y2 * x1) < 0;
+        boolean isOut = x1 > 7 || x1 < 0 || y1 > 7 || y1 < 0 || x2 > 7 || x2 < 0 || y2 > 7 || y2 < 0;
         boolean isNotDiagonal = Math.abs(x2 - x1) - Math.abs(y2 - y1) != 0;
-        if (isNotPositive || isNotDiagonal || isOut) {
+        if (isOut || isNotDiagonal) {
             return rsl;
         }
         rsl = Math.abs(x2 - x1);
