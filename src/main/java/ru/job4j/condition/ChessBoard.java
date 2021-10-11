772164c0cd995445d5594e4409a3ae26e6b76da5
kosintsev <andrejkosincev@gmail.com>
2021-10-11 10:13:07 +0700

    refactoring - 4.3.6. Math.abs [#373616 #214262]

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -3,14 +3,17 @@ package ru.job4j.condition;
 public class ChessBoard {
 
     public static int way(int x1, int y1, int x2, int y2) {
-        int rsl = 0;
-        boolean isOut = x1 > 7 || x1 < 0 || y1 > 7 || y1 < 0 || x2 > 7 || x2 < 0 || y2 > 7 || y2 < 0;
-        boolean isNotDiagonal = Math.abs(x2 - x1) - Math.abs(y2 - y1) != 0;
-        if (isOut || isNotDiagonal) {
-            return rsl;
+        if (isOut(x1, y1, x2, y2) || isNotDiagonal(x1, y1, x2, y2)) {
+            return 0;
         }
-        rsl = Math.abs(x2 - x1);
-        rsl = rsl == 0 ? Math.abs(y2 - y1) : rsl;
-        return rsl;
+        return Math.abs(x2 - x1);
+    }
+
+    public static boolean isOut(int x1, int y1, int x2, int y2) {
+        return x1 > 7 || x1 < 0 || y1 > 7 || y1 < 0 || x2 > 7 || x2 < 0 || y2 > 7 || y2 < 0;
+    }
+
+    public static boolean isNotDiagonal(int x1, int y1, int x2, int y2) {
+        return Math.abs(x2 - x1) - Math.abs(y2 - y1) != 0;
     }
 }
