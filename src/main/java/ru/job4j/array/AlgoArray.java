de1570396b8dc4b88b0e1fd57ba07b428b7064ce
kosintsev <andrejkosincev@gmail.com>
2021-12-03 10:19:18 +0700

    leaded to compliance with requirements - 6.2. Упорядочить массив. [#4441 #235019]

--- src/main/java/ru/job4j/array/AlgoArray.java
+++ src/main/java/ru/job4j/array/AlgoArray.java
@@ -3,19 +3,15 @@ package ru.job4j.array;
 public class AlgoArray {
     public static void main(String[] args) {
         int[] array = new int[] {5, 3, 2, 1, 4};
-        boolean isSorted;
-        int bubble;
-        do {
-            isSorted = true;
-            for (int i = 0; i < array.length - 1; i++) {
-                if (array[i] > array[i + 1]) {
-                    isSorted = false;
-                    bubble = array[i];
-                    array[i] = array[i + 1];
-                    array[i + 1] = bubble;
-                }
-            }
-        } while (!isSorted);
+        int temp = array[0];
+        array[0] = array[3];
+        array[3] = temp;
+        temp = array[1];
+        array[1] = array[2];
+        array[2] = temp;
+        temp = array[3];
+        array[3] = array[4];
+        array[4] = temp;
         for (int element : array) {
             System.out.println(element);
         }
